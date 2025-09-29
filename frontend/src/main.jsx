537cf9e5d4ac26a929e7c665f6ab100fb6cc32fa
TheMadamButterfly <jdansby@madetoheal.world>
2025-09-29 13:18:05 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
-import './theme.css';
-import './theme.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './theme.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(<App />);
